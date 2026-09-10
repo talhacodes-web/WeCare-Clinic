67a5298992d24a3f24071c33028af9e79b62e832
Talha <talhasub57@gmail.com>
2026-09-10 15:57:44 +0500

    Update server port for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ const connectDB = require("./src/config/db");
 
 connectDB();
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Server started on port 3000");
 }); 
