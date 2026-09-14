221ead855b15ad1e520fef9dae635518152a9702
Talha <talhasub57@gmail.com>
2026-09-14 18:27:32 +0500

    Fix cookie code

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -34,7 +34,7 @@ const setRefreshCookie = (res, token) => {
     res.cookie("refreshToken", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
     });
 };
